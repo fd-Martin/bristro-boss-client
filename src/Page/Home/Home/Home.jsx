ebabe6b09da45e9d761fb17ac54db9be5a47d5f8
fd-Martin <fdmosharof@gmail.com>
2024-04-28 07:50:17 +0600

    Create Simple Top Banner React Responsive Carousel

--- src/Page/Home/Home/Home.jsx
+++ src/Page/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Bannar from '../Bannar/Bannar';
 
 const Home = () => {
     return (
         <div>
-            <h1>this is home</h1>
+           <Bannar></Bannar>
         </div>
     );
 };
--- src/Page/Shared/Navbar.jsx
+++ src/Page/Shared/Navbar.jsx
@@ -3,18 +3,12 @@ import React from 'react';
 const Navbar = () => {
     const navOptions = <>
         <li><a>Item 1</a></li>
-        <li>
-            <a>Parent</a>
-            <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-            </ul>
-        </li>
+        
         <li><a>Item 3</a></li>
     </>
     return (
         <>
-            <div className="navbar bg-base-100">
+            <div className="navbar fixed z-50  max-w-screen-xl bg-opacity-30 bg-black text-white">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
