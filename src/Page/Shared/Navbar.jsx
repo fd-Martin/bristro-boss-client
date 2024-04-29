7cda50c6713417f78ea7f59488193d85df3a2fe6
fd-Martin <fdmosharof@gmail.com>
2024-04-29 09:56:30 +0600

    Dynamic Page Title By Route Using React Helmet

--- src/Page/Shared/Navbar.jsx
+++ src/Page/Shared/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const navOptions = <>
-        <li><a>Item 1</a></li>
-        
-        <li><a>Item 3</a></li>
+
+
+        <li><Link to='/'>Home</Link> </li>
+        <li><Link to='/menu'>Our Menu</Link> </li>
     </>
     return (
         <>
