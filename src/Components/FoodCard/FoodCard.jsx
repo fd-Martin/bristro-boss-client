37479f4af4ffef6629617c79d577aacf76dc07db
fd-Martin <fdmosharof@gmail.com>
2024-04-30 09:03:29 +0600

    Load Menu Data From Server

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const FoodCard = ({item}) => {
+const FoodCard = ({ item }) => {
     const { image, recipe, price, name } = item;
     return (
         <div>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <figure><img src={image} alt="Shoes" /></figure>
                 <p className='bg-slate-900 absolute right-0  mr-4 mt-4 px-4'>$ {price}</p>
-                <div className="card-body text-center">
+                <div className="card-body flex flex-col items-center">
                     <h2 className="card-title">{name}</h2>
                     <p>{recipe}</p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Add to Cart</button>
+                        <button className="btn btn-outline bg-slate-200 border-b-4 border-orange-400 mt-4">Add to Cart</button>
                     </div>
                 </div>
             </div>
