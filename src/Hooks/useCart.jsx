8b442dc1707d0e8bbe9f829e2ab0a7484f9f0923
fd-Martin <fdmosharof@gmail.com>
2024-05-04 21:18:51 +0600

    Simple Overview Of React Query And Load Data

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -1,4 +1,17 @@
-const useCart =email=>{
+import { useQuery } from '@tanstack/react-query'
+import { useContext } from 'react';
+import { AuthContext } from '../Prodiver/AuthProvider';
+const useCart = () => {
+    const { user } = useContext(AuthContext);
+
+    const { isLoading, refetch, data: cart = [] } = useQuery({
+        queryKey: ['carts', user?.email],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
+            return res.json()
+        },
+    })
+    return [cart, isLoading, refetch]
 
 }
 export default useCart;
